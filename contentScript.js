6d9e60433b2ff76cb4f44ee9eda6cc85f368bb7c
tejaleko <86977116+tejaleko@users.noreply.github.com>
2025-10-07 06:27:07 -0400

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -10,3 +10,28 @@ s.onload = function() {
 c.rel = "stylesheet";
 c.href = chrome.runtime.getURL('spotifyEventsScheduler.css');
 (document.head||document.documentElement).appendChild(c);
+const SCRIPT_FLAG = 'spotifyEventsSchedulerInjected';
+const STYLE_ID = 'spotify-events-scheduler-style';
+
+const root = document.head || document.documentElement;
+
+if (!document.documentElement.dataset[SCRIPT_FLAG]) {
+  document.documentElement.dataset[SCRIPT_FLAG] = 'true';
+
+  const script = document.createElement('script');
+  script.src = chrome.runtime.getURL('spotifyEventsScheduler.js');
+  script.onload = function () {
+    script.remove();
+  };
+
+  root.appendChild(script);
+}
+
+if (!document.getElementById(STYLE_ID)) {
+  const stylesheet = document.createElement('link');
+  stylesheet.id = STYLE_ID;
+  stylesheet.rel = 'stylesheet';
+  stylesheet.href = chrome.runtime.getURL('spotifyEventsScheduler.css');
+
+  root.appendChild(stylesheet);
+}
